6f3626c187ba0dc987d6db98ab9ce314dc4dfe85
matiasnmoyano <moyano.matiasnicolas.mm@gmail.com>
2022-02-13 16:48:26 -0300

    subir archivos y getArchivo y getArchivos

--- api/controllers.js
+++
@@ -1,9 +0,0 @@
-import { getStorage, ref } from "firebase/storage";
-
-const storage = getStorage();
-
-const mountainImagesRef = ref(storage, 'images/mountains.jpg');
-
-export default function uploadFiles () {
-    
-}
